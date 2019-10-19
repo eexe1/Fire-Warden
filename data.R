9fa6fbf2af7c66921ad3a7d4bcdf42b5c65b706b
Evan <devillordking@gmail.com>
2019-10-19 20:23:52 +1100

    add gas station

---
+++ data.R
@@ -0,0 +1,4 @@
+# does not work, so I uploaded file to mapbox directly.
+# gasStations <- rgdal::readOGR("data/California_Natural_Gas_Station.geojson", what = "sp")
+# 
+# pal <- colorNumeric("viridis", NULL)
